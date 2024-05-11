468883d9247324c1efeda7d3b37af307033ad3e1
laygofiona <laygo.fiona@gmail.com>
2024-05-11 14:17:54 -0400

    add instructions on how to run app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       {/* Components can be added here, we can use css grid to design the layout of the components */} 
       {/* will need design/figma pages for reference */}
       {/* added hello world to make sure the app works, feel free to remove all this code */}
+      {/* To run the app on a web browser, in the terminal, type in: npn run dev*/}
+      {/* To quit or stop the app from running, in the terminal, type in: q*/}
       <p>Hello World</p>
     </div>
   )
