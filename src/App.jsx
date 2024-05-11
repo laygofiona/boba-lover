fc2d15311ea5afd767ac9ddd7fe2bc5ed35edb38
laygofiona <laygo.fiona@gmail.com>
2024-05-11 14:15:03 -0400

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      {/* Components can be added here, we can use css grid to design the layout of the components */} 
+      {/* will need design/figma pages for reference */}
+      {/* added hello world to make sure the app works, feel free to remove all this code */}
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default App
