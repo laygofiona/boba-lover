48faddb36e20ef95dffd346c37a9bfb7c2ebeae8
Batool Kazmi <kazmib6@gmail.com>
2024-05-11 21:14:30 -0400

    Created new pages for the bobacards

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import './styles/App.css'
+import BobaCards from './BobaCards'
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      {/* Components can be added here, we can use css grid to design the layout of the components */} 
+      {/* Components can be added here, we can use css grid to design the layout of the components */}
       {/* will need design/figma pages for reference */}
       {/* added hello world to make sure the app works, feel free to remove all this code */}
       {/* To run the app on a web browser, in the terminal, type in: npn run dev*/}
       {/* To quit or stop the app from running, in the terminal, type in: q*/}
       <p>Hello World</p>
+
+      <BobaCards />
     </div>
   )
 }
---
+++ src/BobaCards.jsx
@@ -0,0 +1,14 @@
+// The Header Component -- file is capitalized because it is an component
+import React from 'react'
+import './styles/BobaCards.css'
+
+
+function BobaCards() {
+    return (
+        <div className='cards'>
+            <p> TinderCards 🔥</p>
+        </div>
+    );
+}
+
+export default BobaCards
