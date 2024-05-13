4a39acd076b56d5ce6f8f14e6267a206c9dcb21c
Garv <garv1908@gmail.com>
2024-05-13 12:37:21 +0400

    uninstall old dependencies @material-ui and install new @mui

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import PersonIcon from "@material-ui/icons/Person";
-import ForumIcon from "@material-ui/icons/Forum";
-import IconButton from "@material-ui/core/IconButton";
+
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import IconButton from '@mui/material/IconButton';
 import "./styles/Header.css";
 
 const Header = () => {
