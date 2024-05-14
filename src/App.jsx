0dfc7998044c3fa5d44a1923bfe84b46324a610a
laygofiona <laygo.fiona@gmail.com>
2024-05-14 13:12:33 -0400

    added link to chat page in header and edited chatpage style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Header from "./Header";
 import './styles/App.css'
 import BobaCards from './BobaCards'
+import { Route, Routes } from "react-router-dom"
+import ChatPage from './pages/ChatPage';
 
 function App() {
 
@@ -10,6 +12,13 @@ function App() {
     <div className="App">
       {/* shall we use the BEM naming convention for this project? Yes*/}
       <Header />
+      <div className="routes">
+        <Routes>
+          {/*<Route path="/profile" element={<></>}/>*/}
+          {/* <Route path="/" element={<></>}/>*/}
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
+      </div>
       {/* Components can be added here, we can use css grid to design the layout of the components */}
       {/* will need design/figma pages for reference */}
       {/* added hello world to make sure the app works, feel free to remove all this code */}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
 import PersonIcon from '@mui/icons-material/Person';
 import ForumIcon from '@mui/icons-material/Forum';
 import IconButton from '@mui/material/IconButton';
 import "./styles/Header.css";
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return(
@@ -12,11 +12,12 @@ const Header = () => {
                 <PersonIcon className="header__icon" />
             </IconButton>
             <img src='https://cdn-icons-png.flaticon.com/512/4645/4645924.png'
-            alt='boba logo' className='header__logo'
-            />
-            <IconButton>
-                <ForumIcon className='header__icon' />
-            </IconButton>
+            alt='boba logo' className='header__logo'/>
+            <Link to="/chats">
+                <IconButton>
+                    <ForumIcon className='header__icon' />
+                </IconButton>
+            </Link>
         </div>
     );
 }
