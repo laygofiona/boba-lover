72265768f6d12a917843b05086eae50793b74ace
Batool Kazmi <kazmib6@gmail.com>
2024-05-18 17:37:14 -0400

    added bobaLover logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,9 +4,10 @@ import ForumIcon from '@mui/icons-material/Forum';
 import IconButton from '@mui/material/IconButton';
 import "./styles/Header.css";
 import { Link } from "react-router-dom"
+import logo from './img/logo.png';
 
 const Header = () => {
-    return(
+    return (
         <div className="header">
             <Link to="/profile">
                 <IconButton>
@@ -15,7 +16,9 @@ const Header = () => {
             </Link>
             <Link to="/">
                 <img src='https://cdn-icons-png.flaticon.com/512/4645/4645924.png'
-                 alt='boba logo' className='header__logo'/>
+                    alt='boba logo' className='header__logo' />
+                <img src={logo}
+                    alt='boba logo' className='header__logo' />
             </Link>
             <Link to="/chats">
                 <IconButton>
